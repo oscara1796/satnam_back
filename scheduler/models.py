ac207fc53f817858771a865912c8eae6cade326d
Oscar Carrillo <oscar.carrillo2941@alumnos.udg.mx>
2024-01-30 20:05:14 -0600

    creating schedule app to handle the Schedule feature

---
+++ scheduler/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    day = models.CharField(max_length=20)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
