7e86f58f76bd2989cb4305e39d881454ac533032
Oscar Abraham <oscara1706cl@gmail.com>
2024-02-01 08:37:30 -0600

    trying to save schedule

--- scheduler/views.py
+++ scheduler/views.py
@@ -9,21 +9,21 @@ class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAdminUser]
 
-    def create(self, request, *args, **kwargs):
-        if Event.objects.exists():
-            # If an instance already exists, update it instead of creating a new one
-            return self.update(request, *args, **kwargs)
-        return super(EventViewSet, self).create(request, *args, **kwargs)
+    # def create(self, request, *args, **kwargs):
+    #     if Event.objects.exists():
+    #         # If an instance already exists, update it instead of creating a new one
+    #         return self.update(request, *args, **kwargs)
+    #     return super(EventViewSet, self).create(request, *args, **kwargs)
 
-    def update(self, request, *args, **kwargs):
-        # Ensure there is only one instance
-        event, created = Event.objects.get_or_create(pk=1)
-        serializer = self.get_serializer(event, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+    # def update(self, request, *args, **kwargs):
+    #     # Ensure there is only one instance
+    #     event, created = Event.objects.get_or_create(pk=1)
+    #     serializer = self.get_serializer(event, data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_update(serializer)
 
-        if getattr(event, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to forcibly invalidate the prefetch cache on the instance.
-            event._prefetched_objects_cache = {}
+    #     if getattr(event, '_prefetched_objects_cache', None):
+    #         # If 'prefetch_related' has been applied to a queryset, we need to forcibly invalidate the prefetch cache on the instance.
+    #         event._prefetched_objects_cache = {}
 
-        return Response(serializer.data)
+    #     return Response(serializer.data)
