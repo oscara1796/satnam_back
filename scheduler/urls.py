5bb966c6b076715782255bfc65e34c6dad72d7c0
Oscar Carrillo <oscar.carrillo2941@alumnos.udg.mx>
2024-02-02 17:23:51 -0600

    trying to add scheduler to my project

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('api/events/', EventViewSet.as_view({'get': 'list', 'post': 'create'}), name='events'),
     path('api/events/<int:pk>/', EventViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='event'),
 ]
+
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,29 +1,37 @@
-from rest_framework import viewsets
+
 from rest_framework.response import Response
 from .models import Event
 from .serializers import EventSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, status, permissions
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # Allow GET, HEAD, OPTIONS requests
+            return True
+        return request.user.is_staff
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsStaffOrReadOnly]
 
     def create(self, request, *args, **kwargs):
-        if Event.objects.exists():
-            # If an instance already exists, update it instead of creating a new one
-            return self.update(request, *args, **kwargs)
-        return super(EventViewSet, self).create(request, *args, **kwargs)
+        # Initial validation of the incoming data
+        if isinstance(request.data, list):
+            # Bulk creation case
+            serializer = self.get_serializer(data=request.data, many=True)
+        else:
+            # Single creation case
+            serializer = self.get_serializer(data=request.data)
 
-    def update(self, request, *args, **kwargs):
-        # Ensure there is only one instance
-        event, created = Event.objects.get_or_create(pk=1)
-        serializer = self.get_serializer(event, data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
 
-        if getattr(event, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to forcibly invalidate the prefetch cache on the instance.
-            event._prefetched_objects_cache = {}
+        # Data is valid, so now we can safely delete old events
+        Event.objects.all().delete()
 
-        return Response(serializer.data)
+        # Perform the creation of new events
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
