60105939b683a0f80b8521e9a44243d2319545c4
Oscar <oscara1706cl@gmail.com>
2024-06-23 16:27:35 -0600

    adding S3 bucket

--- satnam/settings.py
+++ satnam/settings.py
@@ -282,6 +282,12 @@ AWS_STORAGE_BUCKET_NAME = 'satnam-bucket'
 
 if not DEBUG:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
