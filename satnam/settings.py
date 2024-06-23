7b6cfe2e377bce438af208aedd7e674f8442e660
Oscar <oscara1706cl@gmail.com>
2024-06-23 11:55:54 -0600

    trying to make threads to work on heroku

--- satnam/settings.py
+++ satnam/settings.py
@@ -109,6 +109,13 @@ if DEBUG:
         }
     }
 else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
     DATABASE_URL = os.environ.get('DATABASE_URL')
     db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
     DATABASES['default'].update(db_from_env)
