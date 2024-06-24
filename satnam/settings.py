96e873920e7341268fbca7d87212343bb8191649
Oscar Carrillo <oscar.carrillo2941@alumnos.udg.mx>
2024-06-24 10:52:37 -0600

    preparing to merge

--- satnam/settings.py
+++ satnam/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "captcha",
     "sslserver",
     "rest_framework",
     "corsheaders",
