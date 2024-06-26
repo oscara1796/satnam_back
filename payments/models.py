d2eb707ca26f35f7344f008193e3f01242e83c1d
Oscar Carrillo <oscar.carrillo2941@alumnos.udg.mx>
2024-06-26 08:27:07 -0600

    making a plans views

--- payments/models.py
+++ payments/models.py
@@ -8,3 +8,20 @@ class StripeEvent(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     processed_at = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=20)
+
+
+
+
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    features = models.JSONField()
+    metadata = models.JSONField()
+    paypal_plan_id = models.CharField(max_length=255, null=True, blank=True)
+    stripe_product_id = models.CharField(max_length=255, null=True, blank=True)
+    stripe_price_id = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
